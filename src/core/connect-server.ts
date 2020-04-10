771dec61c7b51a5de41520c977650bbb0a998d66
zishone <zishgarces@gmail.com>
2020-04-10 12:38:55 +0800

    Remove logging of db credentials

--- src/core/connect-server.ts
+++ src/core/connect-server.ts
@@ -7,13 +7,15 @@ import { getLogger } from '../utils/get-logger';
 const logger = getLogger(__filename);
 
 export async function connectServer(uri: string, options: MongoClientOptions): Promise<MongoClient> {
+  const loggableUri = uri.substring(0, uri.indexOf('://') + 3) +
+    (uri.includes('@') ? uri.substring(uri.indexOf('@') + 1, uri.indexOf('?')) : uri.substring(uri.indexOf('://') + 3, uri.length));
   try {
-    logger.debug('Connecting to the Server: %s', uri);
-    logger.cli('- Connecting to the Server: %s', uri);
+    logger.debug('Connecting to the Server: %s', loggableUri);
+    logger.cli('- Connecting to the Server: %s', loggableUri);
     return await MongoClient.connect(uri, options);
   } catch (error) {
-    logger.error('Error connecting to the Server: %s', uri);
-    logger.cli('- Error connecting to the Server: %s', uri);
+    logger.error('Error connecting to the Server: %s', loggableUri);
+    logger.cli('- Error connecting to the Server: %s', loggableUri);
     throw error;
   }
 }
