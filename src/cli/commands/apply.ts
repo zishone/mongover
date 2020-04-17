dcbb820a8cdc0b60bedd6aa212341d682b16f2d8
zishone <zishgarces@gmail.com>
2020-04-17 17:36:26 +0800

    Shorten specPath log

--- src/cli/commands/apply.ts
+++ src/cli/commands/apply.ts
@@ -8,7 +8,7 @@ export async function apply(options: MongoverOptions): Promise<void> {
   try {
     logger.cli('Applying Mongover Specification: %s', options.specPath);
     await coreApply(options);
-    logger.cli('Done applying Mongover Specification: %s', options.specPath);
+    logger.cli('Done applying Mongover Specification: %s', options.specPath!.replace(process.cwd(), '.'));
   } catch (error) {
     logger.cli('Error applying Mongover Specification: %O', error);
     throw error;
